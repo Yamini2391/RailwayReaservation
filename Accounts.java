a134d22fc75e9c1fb8fed6a3a374a193849088aa
nisha1611 <nishadeshmukh1611@gmail.com>
2021-04-19 23:58:49 +0530

    username already exists and exit option

--- Accounts.java
+++ Accounts.java
@@ -9,17 +9,29 @@ public class Accounts {
 	Scanner sc=new Scanner(System.in);
 	
 	void createAccount() {
+		int flag=0;
+		do{
+		flag=0;
 		System.out.println("Enter the username you want to use for your account");
 		String userName = sc.nextLine();
+		for(User u: userList) {
+			if(u.userName.equals(userName)) {
+				System.out.println("Already exists. Try a new username.");
+				flag=1;
+				break;
+			}
+		if(flag==0){
 		System.out.println("Enter the password to secure your account");
 		String password = sc.nextLine();
 		User u = new User(userName,password);
 		userList.add(u);
 		u.userMenu();
+		}
+		}while(flag==1);
 	}
 
 	public void login() {
-		int flag=0;
+		int flag=0, trying=0;
 		do{
 			System.out.println("Enter the userName");
 			String userName=sc.nextLine();
@@ -39,7 +51,10 @@ public class Accounts {
 			}
 			else {
 				System.out.println("Please enter proper credentials");
+				System.out.printf("Do you want to keep trying?\n1.Yes\n2.Exit");
+		    	trying = sc.nextInt();
+		    	sc.nextLine();
 			}
-		}while(flag==0);	
+		}while(flag==0 && trying==1);	
 	}
 }
